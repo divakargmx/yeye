3bab614ceaee78b2123f4bd6c474afbda24e5834
Divakar <132250823+divakargmx@users.noreply.github.com>
2023-09-08 12:23:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LedControl from './LedControl';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LedControl />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
